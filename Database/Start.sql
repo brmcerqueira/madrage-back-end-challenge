d9056c167c9d756f69d3b55c4b1f449c2f3a6687
bruno <bruno.cerqueira@fieldcorp.com.br>
2020-03-01 12:08:01 -0300

    Criação do TreeNode.cs

--- Database/Start.sql
+++ Database/Start.sql
@@ -1,23 +1,27 @@
 create sequence users_seq;
 create sequence menus_seq;
 
-create table if not exists users(
-   id integer not null default nextval ('users_seq'),
+create table users(
+   id int not null default nextval ('users_seq'),
    name varchar(50) not null,
    email varchar(30) not null,
    password varchar(60) not null,
    primary key (id)
-)
+);
 
-create table if not exists menus(
-   id integer not null default nextval ('menus_seq'),
+create table menus(
+   id int not null default nextval ('menus_seq'),
    label varchar(30) not null,
-   parent_id integer references menus(id) on delete cascaded,
+   parent_id int,
    primary key (id)
-)
+);
 
-create table if not exists menus_users(
-   menu_id integer references menus(id) on delete cascaded,
-   user_id integer references users(id) on delete cascaded,
+create table menus_users(
+   menu_id int not null,
+   user_id int not null,
    primary key (menu_id, user_id)
-)
+);
+
+alter table menus add foreign key(parent_id) references menus(id) on delete cascade;
+alter table menus_users add foreign key(menu_id) references menus(id) on delete cascade;
+alter table menus_users add foreign key(user_id) references users(id) on delete cascade;
